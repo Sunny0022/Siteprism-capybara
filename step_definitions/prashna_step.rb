ca94b46824a8aeadd410c713e65cd06e669fd701
Sunny0022 <Sunny0022@users.noreply.github.com>
2020-08-07 18:39:50 +0530

    Update prashna_step.rb

--- step_definitions/prashna_step.rb
+++ step_definitions/prashna_step.rb
@@ -9,21 +9,14 @@ Capybara.default_driver = :selenium
 Given(/^I am on Prashna Site$/) do
   @prashna = PrashnaPage.new
   @prashna.load
-  expect(@prashna).to be_displayed
 end
 
 And(/^I click login tab$/) do
-  @prashna.wait_until_displayed('Home')
   @prashna.wait_until_displayed('Login')
-  @prashna.wait_until_displayed('Signup')
-  expect(@prashna).to have_content('Prashna')
   click_link 'Login'
-  Capybara.default_max_wait_time = 5
-
 end
 
 When(/^I enter the credentials to (.*) and (.*)$/) do |email, pass|
-  Capybara.default_max_wait_time = 5
   @prashna.wait_until_email_id_visible
   @prashna.wait_until_pass_visible
   @prashna.signin_field(email, pass)
@@ -31,11 +24,8 @@ When(/^I enter the credentials to (.*) and (.*)$/) do |email, pass|
 end
 
 Then(/^I should be signed in successfully$/) do
-  Capybara.default_max_wait_time = 5
-  @prashna.wait_until_displayed('Home')
   click_link 'Home'
   @prashna.wait_until_displayed('Logout')
-  Capybara.default_max_wait_time = 5
   click_link 'Logout'
   expect(@prashna).to have_content('Logged out')
 end
@@ -47,44 +37,28 @@ end
 Then(/^I should see all elements of the page$/) do
   @fields = SignUpPage.new
   @fields.load
-  @fields.has_content?('Signup Form')
-  @fields.has_content?('Name')
-  @fields.has_content?('Email')
-  @fields.has_content?('Password')
-  @fields.has_content?('Password Confirmation')
-  Capybara.default_max_wait_time = 5
+  @fields.wait_until_name_visible
+  @fields.wait_until_mail_visible
 end
-
-
-
 
 And(/^I fill with (.*), (.*), (.*) and (.*)$/) do |name, email, passw, confirm_pass|
   @fields.signup_field(name, email, passw, confirm_pass)
   click_button 'Submit'
-  Capybara.default_max_wait_time = 5
 end
 
 And(/^User is signed up successfully$/) do
   expect(@fields).to have_content('Signup successful. Please verify your account, to login')
-  Capybara.default_max_wait_time = 5
 end
 
 
 And(/^I click My question tab$/) do
   click_link 'My Questions'
   click_link 'New Question'
-  Capybara.default_max_wait_time = 5
 end
 
 Then(/^I should see the elements of new question form$/) do
   @question = QuestionsPage.new
   @question.load
-  @question.wait_until_displayed(@question.title)
-  @question.wait_until_displayed(@question.content)
-  @question.wait_until_displayed(@question.topic)
-  Capybara.default_max_wait_time = 5
-  expect(@question.title).to have_content('')
-  expect(@question.topic).to have_content('')
-  expect(@question.content).to have_content('')
+  @question.wait_until_title_visible
   click_link 'Logout'
 end
